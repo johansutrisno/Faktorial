79480ea5af6e1256cbed66dcd81e7874d1c49687
JohanSutrisno_6706161099 <johansutrisno30@gmail.com>
2018-03-26 10:23:21 +0700

    Faktorial v.1.1

---
+++ Faktorial/src/faktorial/Faktorial.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faktorial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class Faktorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner in = new Scanner (System.in);
+        
+        System.out.print("Masukan angka: ");
+        int x = in.nextInt();
+        int start = 2;
+        
+        for (int i = x; i > 0; i--) {
+            for(int j = 0; j < i; j++) System.out.print(" ");
+            for (int j = 0; j < start; j++) System.out.print("*");
+            for (int j = 0; j < i; j++) System.out.print(" ");
+            System.out.println();
+            if (i != 0) start+=2;
+        }
+        
+        for (int i = 0; i < x; i++) {
+            for(int j = 0; j < i; j++) System.out.print(" ");
+            for (int j = 0; j < start; j++) System.out.print("*");
+            for (int j = 0; j < i; j++) System.out.print(" ");
+            System.out.println();
+            start-=2;
+        }
+    }
+    
+}
